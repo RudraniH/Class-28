32fb07e05288015e162e35be36cf53d9b57f3012
RudraniH <89968054+RudraniH@users.noreply.github.com>
2021-11-18 18:10:17 +1100

    Add files via upload

---
+++ Class 28/js/Cannon.js
@@ -0,0 +1,31 @@
+class Cannon{
+  //Properties
+  constructor(x,y,w,h,angle){
+      this.x=x;
+      this.y=y;
+      this.w=w;
+      this.h=h;
+      this.angle=angle;
+  }
+
+  //function
+  display(){
+    if(keyIsDown(RIGHT_ARROW)&&this.angle<0.35){
+      this.angle+=0.2;
+    }
+    if(keyIsDown(LEFT_ARROW)&&this.angle>-1.45){
+      this.angle-=0.2
+    }
+      fill("#676e6a");
+      push();
+      translate(this.x,this.y);
+      rotate(this.angle); //45
+      rect(-10,-20,this.w,this.h);
+      pop();
+
+      arc(this.x-30,this.y+90,140,200,PI,PI*2);
+      noFill();
+  }
+
+
+}
